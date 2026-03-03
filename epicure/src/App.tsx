49dc8f60b86c0961f3409ef3df8e27036f8310df
Rom Shemer <Romshemer2000@gmail.com>
2026-03-03 16:48:49 +0200

    refactor: reorganize Footer folder structure

--- epicure/src/App.tsx
+++ epicure/src/App.tsx
@@ -1,7 +1,7 @@
 //import './App.css'
 import Header from './components/layout/Header/Header'
 import Hero from './components/layout/Sections/HeroSection/Hero'
-import Footer from './components/layout/Sections/FooterSection/Footer'
+import Footer from './components/layout/Footer/Footer'
 
 function App() {
 
