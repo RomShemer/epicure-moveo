0d9cff0f132b823b82a638d8dfa07c4170de4fe4
Rom Shemer <Romshemer2000@gmail.com>
2026-03-04 12:41:55 +0200

    feat: add PopularRestaurantsSection component with styles and swiper integration

--- epicure/src/App.tsx
+++ epicure/src/App.tsx
@@ -2,13 +2,14 @@
 import Header from './components/layout/Header/Header'
 import Hero from './components/layout/Sections/HeroSection/Hero'
 import Footer from './components/layout/Footer/Footer'
-
+import PopularRestaurantsSection from './components/layout/Sections/PopularRestaurantSection/PopularRestaurants'
 function App() {
 
   return (
     <>
     <Header/>
     <Hero/>
+    <PopularRestaurantsSection/>
     <Footer/>
     </>
   )
---
+++ epicure/src/components/layout/Sections/PopularRestaurantSection/PopularRestaurants.tsx
@@ -0,0 +1,48 @@
+import { restaurants } from "../../../../mock/restaurants"
+import RestaurantCard from "./RestaurantCard"
+import styles from "./PopularRestaurantsSection.module.css"
+import { useMediaQuery } from "../../../../hooks/useMediaQuery"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Pagination } from "swiper/modules"
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import chevronIcon from "../../../../assets/ChevronIcon.svg"
+
+const PopularRestaurantsSection = () => {
+
+  const isDesktop = useMediaQuery("(min-width: 1024px)")
+
+  return (
+    <section className={styles.section}>
+      <h2 className={styles.title}>popular restaurant in epicure:</h2>
+
+      <Swiper className={styles.swiperWrapper}
+        /*modules={[Navigation, Pagination]}*/
+        spaceBetween={24}
+        slidesPerView={isDesktop ? 3 : 1.4}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {restaurants.map((restaurant) => (
+          <SwiperSlide key={restaurant.id}>
+            <RestaurantCard restaurant={restaurant} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}
+          onClick={() => { console.log("All Restaurants button clickedß") }}
+        >
+          <div className={styles.buttonContent}>
+            <span className={styles.buttonText}>All Restaurants</span>
+            <img src={chevronIcon} alt="chevron icon" className={styles.buttonIcon} />
+          </div>
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default PopularRestaurantsSection
